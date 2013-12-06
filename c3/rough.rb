123f7d0f24a65f0b15f8d39bbef04006aa935402
sgpl <sharad45@gmail.com>
2013-12-06 13:15:25 +0300

    ...

---
+++ c3/rough.rb
@@ -0,0 +1,30 @@
+puts "x" > "y" # returns boolean
+puts "x" < "y" # returns boolean
+# ^ characters are stored as numbers inside computer's memory. 
+
+puts ?x # returns numbers that represents x
+puts ?A
+num1 = ?A
+
+puts 120.chr
+puts num1.chr
+
+## read up more about this on: 
+
+# http://en.wikipedia.org/ wiki/ASCII.
+
+# string literals, etc 
+
+xx = %q{
+this is a multiline
+string literal
+}
+
+yy = <<END_OF_STRING_LITERAL
+this is a multiline
+string literal
+END_OF_STRING_LITERAL
+
+puts "x" * 10
+
+
