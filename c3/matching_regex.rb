a9a41f622aa4a9a3d6f9d7f7c52743c2341698b1
sgpl <sharad45@gmail.com>
2013-12-16 22:26:40 +0300

    covered a page or two...dead tired from the day...to be continued tomorrow

--- c3/matching_regex.rb
+++ c3/matching_regex.rb
@@ -2,3 +2,28 @@ _xxx = "the quick brown fox jumps over the lazy dog"
 
 puts "string has vowels" if "this is a test" =~ /[aeiou]/
 puts "This string has all the alphabets" if _xxx =~/[a-z]/
+
+
+puts "string contains no digits" unless "this txt" =~/0-9]/
+
+
+puts "string has vowels" if _xxx.match(/[aeiou]/)
+
+### this bit of code is not quite clear. 
+# need to review what is happening here. 
+
+x = "This is a test".match(/(\w+) (\w+)/)
+puts x
+puts x[0]
+puts x[1]
+puts x[2]
+puts x[3]
+
+puts "----"
+
+xx = "This is a test".match(/(\w+)(\w+)/)
+puts xx
+puts xx[0]
+puts xx[1]
+puts xx[2]
+puts xx[3]
