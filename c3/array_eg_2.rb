537321913885e37ed2869881e1cd18aa3acf1e85
sgpl <sharad45@gmail.com>
2013-12-18 23:11:17 +0300

    still not getting the proper hang of inspect and how exactly the strings are getting outputted as arrays. need to play around more with characters of regex, and read up online/docs of inspect

--- c3/array_eg_2.rb
+++ c3/array_eg_2.rb
@@ -1,8 +1,19 @@
+
+# with scan we initially used a block of code that accept each char
+# and displayed them on the screen. 
+
+# scan WITHOUT a block of code returns an array of all the matching parts 
+# of a string, so:
+
 puts "this is a test".scan(/\w/).join(',')
 
-
-# puts "Short sentence. Another. No More.".split(/\./).inspect
+# --------
 
 x = "this is a test".scan(/\w/).join(',')
 
 print x 
+
+
+puts "Short sentence. Another. No More.".split(/\./).inspect
+
+puts "words with lots of spaces".split(/\s+/).inspect
