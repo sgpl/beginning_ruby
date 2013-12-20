9acc1037c22eddc2284fce8d40acf6fe2cc74403
sgpl <sharad45@gmail.com>
2013-12-20 23:16:31 +0300

    added other functionality: word count, sentence count, and char count w/o spaces

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -11,10 +11,23 @@ char_count = text.length
 # i was replacing only single spaces, not '  ' or '        ', etc. 
 char_nospace = text.gsub(/\s+/,'').length
 
+sentence_count = 0 
+text.scan(/\.|\?|!/) { |match| sentence_count += 1}
+# need to escape . and ? with \ to avoid errors as . is anychar in regular exp.
+# initially i had just used \. and forgotten the \? or !
+# also book uses text.split(/\.|\?|!/).length which is shorter i guess.
+word_count = 0 
+text.scan(/\w+/) {|match| word_count += 1}
+
+
 
 puts "#{line_count}  Lines"
 puts "#{char_count} Characters"
 puts "#{char_nospace} Characters (excluding spaces)" 
+puts "#{word_count} Words"
+puts "#{sentence_count} Sentences"
+puts "#{text.split.length} Actual Word Count"
+
 
 =begin
 
@@ -35,6 +48,18 @@ File.open("text.txt").each do |line|
 	# 	text variable contains entire file contents 
 	# 	in a single line to use. 
 end
+
+
+_new_var = text.gsub(/\s+/,'')
+counter = 0 
+_new_var.scan(/\./) { |match| puts match;counter += 1  }
+puts counter
+# ^ counting the number of sentences. 
+
+_str1 = "this is a level-headed decision"
+_str1.scan(/\w+/).length  => 6 # counts alphanumeric characters. 
+_str1.split.length  => 5
+
 
 
 =end
