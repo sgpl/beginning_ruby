8ff5e52806274d589e9dd623fa16b1abddc5b020
sgpl <sharad45@gmail.com>
2013-12-20 21:51:22 +0300

    end of chapter 3. need to get a better hang of procs/lambdas and symbols, and well need to review all the stuff i've learnt about in this chapter

--- part2_c3/symbols.rb
+++ part2_c3/symbols.rb
@@ -11,3 +11,24 @@ puts "PANIC" if current_situation == :bad
 # creates a new object stored in memory. 
 # symbols are single reference values only initialized once. 
 
+# symbols are particularly useful when creatig hashes and you 
+# want a distinction between keys and values. 
+
+# eg: 
+
+s = {:key => 'value'}
+
+# also useful in being consistent when deciding which key names to use: 
+
+person_1 = {:name => 'Adam', :age => 10, :height => 177, :gender => :male}
+person_2 = {:name => 'Jessica', :age => 20, :height => 127, :gender => :female} 
+
+# symbols can be thought of as less flexible strings that are used as 
+# identifiers. 
+
+# changing stuff to other classes:
+# .to_i	integer 
+# .to_s	string
+# .to_f	float
+# .to_a	array
+# .to_sym 	symbol
