4f521a53f32079d5922aa6f4c040a47495e7b719
sgpl <sharad45@gmail.com>
2013-12-22 18:41:50 +0300

    inheritance

---
+++ chapter6/inheritance.rb
@@ -0,0 +1,26 @@
+=begin 
+# inheritance. 
+
+- ruby doesn't support multiple inheritance. rest is pretty self explainatory. 
+- 
+
+=end
+
+class ParentClass
+  	def method1
+    	puts "Hello from method1 in the parent class"
+  	end
+  	def method2
+    	puts "Hello from method2 in the parent class"
+	end 
+end
+
+class ChildClass < ParentClass
+	def method2
+    	puts "Hello from method2 in the child class"
+  	end
+end
+
+my_object = ChildClass.new
+my_object.method1
+my_object.method2
