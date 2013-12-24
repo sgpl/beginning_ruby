28153fe3ef156de6e0c1d79938abce31ecfb3d2f
sgpl <sharad45@gmail.com>
2013-12-24 16:50:33 +0300

    encapsulation example

---
+++ chapter6/encapsulation.rb
@@ -0,0 +1,26 @@
+# Encapsulation is the ability for an object to have certain methods
+# 	and attributes available for use publicly (from any section of 
+# 	code), but for others to be visible only within the class 
+# 	itself or by other objects of the same class.
+
+# eg:
+class Person
+
+	def initialize(name)
+		set_name(name)
+	end
+
+	def name
+		@first_name + " " + @last_name
+	end
+
+	def set_name(name)
+		first_name, last_name = name.split(/\s+/)
+		set_first_name(first_name)
+		set_last_name(last_name)
+	end
+
+
+
+
+end
