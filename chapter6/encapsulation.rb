8a6c70de4b41240c61aaec7f8a20cbc4f6e56bed
sgpl <sharad45@gmail.com>
2013-12-25 12:37:31 +0300

    ..

--- chapter6/encapsulation.rb
+++ chapter6/encapsulation.rb
@@ -3,7 +3,7 @@
 # 	code), but for others to be visible only within the class 
 # 	itself or by other objects of the same class.
 
-# eg:
+# eg1 cs eg2::
 class Person
 
 	def initialize(name)
@@ -13,6 +13,38 @@ class Person
 	def name
 		@first_name + " " + @last_name
 	end
+
+	def set_name(name)
+		first_name, last_name = name.split(/\s+/)
+		set_first_name(first_name)
+		set_last_name(last_name)
+	end
+
+	def set_first_name(name)
+		@first_name = name
+	end
+
+	def set_last_name(name)
+		@last_name = name
+	end
+
+
+end
+
+
+# eg2: 
+
+class Person
+
+	def initialize(name)
+		set_name(name)
+	end
+
+	def name
+		@first_name + " " + @last_name
+	end
+
+	private
 
 	def set_name(name)
 		first_name, last_name = name.split(/\s+/)
