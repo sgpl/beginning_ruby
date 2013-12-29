cdc3492f538121414f8cae92fc3f932680e2b0e7
sgpl <sharad45@gmail.com>
2013-12-29 22:35:40 +0300

    file open...

--- chapter9/more_file.rb
+++ chapter9/more_file.rb
@@ -3,6 +3,7 @@
 
 File.open("dummy_data.txt").each do |line| 
 	print line.split(','), "\n"
+	# print line.split(',')[0], " is a ", line.split(',')[1], ". Gender: ", line.split(',')[2], " and age:  ",line.split(',')[3], "\n"
 	
 end
 
