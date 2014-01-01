b7c8d4802d41136c3dd47c9a9e1be25a57f07798
sgpl <sharad45@gmail.com>
2014-01-01 17:06:59 +0300

    making changes to/correcting old code

--- chapter9/file_time_eg.rb
+++ chapter9/file_time_eg.rb
@@ -1,7 +1,11 @@
 f = File.new("logfile.txt", "a")
 10.times do 
 	f.puts Time.now
+	f.puts "\n"
 	sleep 3
-	f.close
 end
 
+f.close
+
+# 'a' appends a line to the file. 
+# 
--- chapter9/file_writing.rb
+++ chapter9/file_writing.rb
@@ -1,3 +1,7 @@
 File.open("dummy_data.txt", "w") do |f|
 	f.puts "This is a test!!"
 end
+
+
+# diff letters for different actions: 
+
