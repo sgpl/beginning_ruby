352ed34012a8b1d89dc2c339b1ea83eb52984043
sgpl <sharad45@gmail.com>
2014-01-28 00:29:56 +0300

    still working with YAML. figuring out diff. between dump vs load.

--- db_chapter9/yaml_example.rb
+++ db_chapter9/yaml_example.rb
@@ -1,3 +1,30 @@
+# # yaml example
+# require 'yaml'
+
+# class Person
+# 	attr_accessor :name, :age, :gender
+# end
+
+
+# fred = Person.new 
+# fred.name = "Fred Boggs"
+# fred.age = 45
+
+# laura = Person.new 
+# laura.name = "Laura Boggler"
+# laura.age = 25
+# laura.gender = "Female"
+
+
+# betty = Person.new 
+# betty.name = "Betty Morgan"
+# betty.age = 27
+# betty.gender = "Female"
+
+# test_data = [ fred, betty, laura]
+# puts YAML::dump(test_data)
+
+# --------------------
 
 require 'yaml'
 
@@ -6,14 +33,17 @@ class Person
 end
 
 
-fred = Person.new 
-fred.name = "Fred Boggs"
-fred.age = 45
+yaml_string = <<END_OF_DATA
+---
+- !ruby/object:Person
+  age: 45
+  name: Jimmy
+- !ruby/object:Person
+  age: 23
+  name: Jessica Smith
+END_OF_DATA
 
-laura = Person.new 
-laura.name = "Laura Boggler"
-laura.age = 25
-laura.gender = "Female"
-
-test_data = [ fred, laura]
-puts YAML::dump(test_data)
+test_data2 = YAML::load(yaml_string)
+puts test_data2[0].name
+puts test_data2[0].age
+puts test_data2[1].name
