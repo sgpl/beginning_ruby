3419f7bd876d0d995564855e9d39c1cdc85e2f91
sgpl <sharad45@gmail.com>
2014-01-29 22:32:55 +0300

    testing out some old code in a scratch pad

---
+++ scratch_pad.rb
@@ -0,0 +1,23 @@
+require 'profile'
+class Calculator
+        def self.count_to_large_number
+                x = 0 
+                100000.times { x += 1}
+        end
+
+        def self.count_to_small_number
+                x = 0 
+                1000.times { x += 1}
+        end
+end
+
+Calculator.count_to_small_number
+Calculator.count_to_large_number
+
+
+
+
+
+# Since I just came off work, and a hectic two hour schedule, I'm technically cheating on my resolution to commit some code everyday by commiting this piece of writing. 
+# Anyway going to sleep for a bit so that I can stay awake later in the night.
+# Happy New Year 2014!!
