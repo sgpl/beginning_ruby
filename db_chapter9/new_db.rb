059953b0541bdb01f7b5d723dc2772f97c0f5e4e
sgpl <sharad45@gmail.com>
2014-01-31 09:02:41 +0300

    created a program to create/read/ search and quit a SQLite3 database. It worksgit status

--- db_chapter9/new_db.rb
+++ db_chapter9/new_db.rb
@@ -20,3 +20,55 @@ def create_table
 			age integer)
 			}
 end
+
+def add_person
+	puts "Enter name:"
+	name = gets.chomp
+	puts "Enter job:"
+	job = gets.chomp
+	puts "Enter gender:"
+	gender = gets.chomp
+	puts "Enter age"
+	age = gets.chomp
+	$db.execute("INSERT INTO people (name, job, gender, age) VALUES (?, ?, ?, ?)", name, job, gender, age)
+end
+
+def find_person
+	puts "Enter name or ID of person to find:"
+	id = gets.chomp
+
+	person = $db.execute("SELECT * FROM people WHERE name = ? OR id = ?", id, id.to_i).first
+	
+	unless person
+		puts "No result found."
+		return
+	end
+
+	puts %Q{
+Name: #{person['name']}
+Job: #{person['job']} 
+Gender: #{person['gender']} 
+Age: #{person['age']}}
+end
+
+
+loop do 
+	puts %q{Please select an option:
+
+ 1. Create person table
+ 2. Add a person
+ 3. Look for a person
+ 4. Quit}
+
+	case gets.chomp
+	when '1'
+		create_table
+	when '2'
+		add_person
+	when '3'
+		find_person
+	when '4'
+		disconnect_and_quit
+	end
+end
+
