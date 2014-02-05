d9f813ad6fd774d2a0cf7b13588a4e57614f53ce
sgpl <sharad45@gmail.com>
2014-02-06 01:03:29 +0300

    updated code to put add_accessor method inside a class; added comments.

--- chapter11/class_eval_eg.rb
+++ chapter11/class_eval_eg.rb
@@ -14,3 +14,32 @@ add_accessor_to_person :gender
 person.name = "Sharad Gopal"
 person.gender = "Male"
 puts "#{person.name} is #{person.gender}"
+
+# add_accessor_to_person method is used to add accessors dynamically to the 
+# Person class. 
+# we are essentially creating name and gender accessors from scratch. 
+
+
+# string interpolation makes eval methods powerful tools for generating 
+# different features on the fly. 
+
+# can also make it super easy by making add_accessor_to_person part of class:
+
+class Class
+    def add_accessor(accessor_name)
+        self.class_eval %Q{
+            attr_accessor :#{accessor_name}
+		} 
+	end
+end
+
+
+class Person
+end
+
+person = Person.new
+Person.add_accessor :name
+Person.add_accessor :gender
+person.name = "James Bond"
+person.gender = "Superman"
+puts "#{person.name} is #{person.gender}"
