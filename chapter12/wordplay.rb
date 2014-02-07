2b55a27c4ddcf7e6e7f071343797342d7017993e
sgpl <sharad45@gmail.com>
2014-02-08 00:25:39 +0300

    added sentence splitting method

--- chapter12/wordplay.rb
+++ chapter12/wordplay.rb
@@ -1,2 +1,13 @@
 class WordPlay
 end
+
+class String
+	def sentences
+		gsub(/\n|\r/, ' ').split(/\.\s*/)
+	end
+end
+
+
+p %q{Hello. This is a test of
+basic sentence splitting. It
+even works over multiple lines.}.sentences
