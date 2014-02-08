cc9eb054fcd6b48fa45b4d04e2847fd64b4cc95e
sgpl <sharad45@gmail.com>
2014-02-09 01:10:23 +0300

    edited code to include a test for a list of words(hot_words)

--- chapter12/wordplay.rb
+++ chapter12/wordplay.rb
@@ -12,8 +12,18 @@ class String
 end
 
 
-p %q{Hello. This is a test of
-basic sentence splitting. It
-even works over multiple lines.}.sentences
+# p %q{Hello. This is a test of
+# basic sentence splitting. It
+# even works over multiple lines.}.sentences
 
-p "This is a test of words' capabilities".words
+# p "This is a test of words' capabilities".words
+
+# p %q{Hello. This is a test of basic sentence splitting. It
+# even works on multiple lines}.sentences[1].words[3] # simple array access
+
+hot_words = %w{ruby test}
+my_string = "This is a test. Dull sentence here. Ruby is great. So is cake."
+my_string.sentences.find_all do |s|
+	s.downcase.words.any? {|word| hot_words.include?(word)}
+end
+
