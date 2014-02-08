5ca96d2b4094efedf52bdb0967d67fe4aba3d3c3
sgpl <sharad45@gmail.com>
2014-02-08 09:41:49 +0300

    update wordplay

--- chapter12/wordplay.rb
+++ chapter12/wordplay.rb
@@ -5,9 +5,15 @@ class String
 	def sentences
 		gsub(/\n|\r/, ' ').split(/\.\s*/)
 	end
+
+	def words
+		scan(/\w[\w\'\-]*/)
+	end
 end
 
 
 p %q{Hello. This is a test of
 basic sentence splitting. It
 even works over multiple lines.}.sentences
+
+p "This is a test of words' capabilities".words
