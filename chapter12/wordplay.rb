03edb69e2195ff4c9fad251ce0eae481a2ed81b1
sgpl <sharad45@gmail.com>
2014-02-12 01:43:06 +0300

    added method to switch pronouns

--- chapter12/wordplay.rb
+++ chapter12/wordplay.rb
@@ -27,11 +27,31 @@ my_string.sentences.find_all do |s|
 	s.downcase.words.any? {|word| p hot_words.include?(word)}
 end
 
-# example of a sentence sorting/ranking function 
+# example of a sentence sorting/ranking function. both the inputs are arrays here. 
 def self.best_sentence(sentences, desired_words)
 	ranked_sentences = sentences.sort_by do |s|
 		s.words.length - (s.downcase.words - desired_words).length
 	end
 
 	ranked_sentences.last
+end
+
+def self.switch_pronouns(text)
+	text.gsub(/\b(I am|You are|I|Your|My)\b/i) do |pronoun|
+	case pronoun.downcase
+
+		when "i"
+			"you"	 
+		when "you"
+			"I"	  
+		when "i am"
+			"you are"	   
+		when "you are"
+			"i am"	    
+		when "your"
+			"my"	      
+		when "my"
+			"your"
+	end	      
+	end
 end
