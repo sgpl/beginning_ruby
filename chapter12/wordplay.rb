9d534a23ff3cf40babc5d01bcbbb619b814ad361
sgpl <sharad45@gmail.com>
2014-02-12 13:39:19 +0300

    refactored code; added some manual test cases

--- chapter12/wordplay.rb
+++ chapter12/wordplay.rb
@@ -1,6 +1,3 @@
-class WordPlay
-end
-
 class String
 	def sentences
 		gsub(/\n|\r/, ' ').split(/\.\s*/)
@@ -27,31 +24,39 @@ my_string.sentences.find_all do |s|
 	s.downcase.words.any? {|word| p hot_words.include?(word)}
 end
 
-# example of a sentence sorting/ranking function. both the inputs are arrays here. 
-def self.best_sentence(sentences, desired_words)
-	ranked_sentences = sentences.sort_by do |s|
-		s.words.length - (s.downcase.words - desired_words).length
+
+class WordPlay
+	# example of a sentence sorting/ranking function. both the inputs are arrays here. 
+	def self.best_sentence(sentences, desired_words)
+		ranked_sentences = sentences.sort_by do |s|
+			s.words.length - (s.downcase.words - desired_words).length
+		end
+
+		ranked_sentences.last
 	end
 
-	ranked_sentences.last
-end
-
-def self.switch_pronouns(text)
-	text.gsub(/\b(I am|You are|I|Your|My)\b/i) do |pronoun|
-	case pronoun.downcase
-
-		when "i"
-			"you"	 
-		when "you"
-			"I"	  
-		when "i am"
-			"you are"	   
-		when "you are"
-			"i am"	    
-		when "your"
-			"my"	      
-		when "my"
-			"your"
-	end	      
+	def self.switch_pronouns(text)
+		text.gsub(/\b(I am|You are|I|Your|My)\b/i) do |pronoun|
+			case pronoun.downcase
+			when "i"
+				"you"
+			when "you"
+				"I"
+			when "i am"
+				"you are"
+			when "you are"
+				"i am"
+			when "your"
+				"my"
+			when "my"
+				"your"
+			end	      
+		end
 	end
 end
+
+puts WordPlay.switch_pronouns("You are pretty?")
+puts WordPlay.switch_pronouns("Your cat is fighting with my cat")
+puts WordPlay.switch_pronouns("You are my robot")
+
+
