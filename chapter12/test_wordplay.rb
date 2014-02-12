6656a5004a405ba06010b04df51185bf4f8c7f18
sgpl <sharad45@gmail.com>
2014-02-12 16:05:23 +0300

    updated test assertions

--- chapter12/test_wordplay.rb
+++ chapter12/test_wordplay.rb
@@ -1,15 +1,21 @@
 require 'test/unit'
-require 'wordplay'
+require './wordplay'
 
 class TestWordPlay < Test::Unit::TestCase
 
 	def test_sentences
 		assert_equal(["a", "b", "c d", "e f g"], "a. b. c d. e f g.".sentences)
 		
-		test_text = %q {Hello. This is a test of 
+		test_text = %q{Hello. This is a test of 
 			sentence separation. This is the end 
 			of the test.}
 			assert_equal("This is the end of the test", test_text.sentences[2])
 	end
+
+	def test_words
+		assert_equal(%w{this is a test}, "this is a test".words)
+		assert_equal(%w{these are mostly words}, "these are, mostly, words".words)
+	end
+
 
 end
