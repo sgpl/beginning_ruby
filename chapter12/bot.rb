cb1d18e5a0773c6034eba0d9ac1e742807abdf9a
sgpl <sharad45@gmail.com>
2014-02-15 16:20:02 +0300

    added to the bot class

--- chapter12/bot.rb
+++ chapter12/bot.rb
@@ -24,9 +24,33 @@ class Bot
 	end
 
 	private
-
 	def random_response(key)
 		random_index = rand(@data[:responses][key].length)
 		@data[:responses][key][random_index].gsub(/\[name\]/, @name)
 	end
+
+	public
+	def response_to(input)
+		prepared_input = preprocess(input).downcase
+		sentence = best_sentence(prepared_input)
+	end
+
+	private
+	def preprocess(input)
+		perform_substitutions input
+	end
+
+	def perform_substitutions(input)
+		@data[:presubs].each { |s| input.gsub!(s[0], s[1])}
+		input
+	end
+
+	def best_sentence(input)
+		hot_words = @data[:responses].keys.select do |k|
+			k.class == String && k ~= /^\w+$/
+		end
+
+		WordPlay.best_sentence(input.sentences, hot_words)
+	end
+
 end
