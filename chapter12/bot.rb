238f0455f2860b4182bdf77f41ca8826d8e4f6c6
sgpl <sharad45@gmail.com>
2014-02-15 15:18:53 +0300

    updated bot.rb with functions for greetings and farewell

--- chapter12/bot.rb
+++ chapter12/bot.rb
@@ -2,14 +2,31 @@ require 'yaml'
 require 'wordplay'
 
 class Bot
-	attr_reader :name
+	attr_reader :name # readable attribute called name
 
 	def initialize(options)
-		@name = options[:name] || "Unnamed Bot"
+		@name = options[:name] || "Unnamed Bot" # name = name otherwise it is an Unnamed bot
 		begin
 			@data = YAML.load(File.read(options[:data_file]))
 		rescue 
 			raise "Can't load bot data"
 		end
+	end
+
+	def greeting
+		# @data[:response][:greeting][rand(@data[:response][:greeting].length)]	
+		random_response :greeting
+	end
+
+	def farewell
+		# @data[:response][:farewell][rand(@data[:response][:farewell].length)]
+		random_response :farewell
+	end
+
+	private
+
+	def random_response(key)
+		random_index = rand(@data[:responses][key].length)
+		@data[:responses][key][random_index].gsub(/\[name\]/, @name)
 	end
 end
